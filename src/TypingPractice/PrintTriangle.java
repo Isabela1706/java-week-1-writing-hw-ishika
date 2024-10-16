f8a5a4ac3364aa1c1337ad0171322e31f517e4c8
Isabela1706 <id1317238@gmail.com>
2024-10-16 22:34:41 +0100

    My homework typing practice

---
+++ src/TypingPractice/PrintTriangle.java
@@ -0,0 +1,22 @@
+package TypingPractice;
+
+/**
+ * 4. Write a Java Program to print below triangle
+ * *
+ * **
+ * ***
+ * ****
+ * *****
+ * ******
+ */
+
+public class PrintTriangle {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("******");
+        System.out.println("*******");
+    }
+}
---
+++ src/TypingPractice/UsaFlag.java
@@ -0,0 +1,27 @@
+package TypingPractice;
+
+/**
+ * 6. Write a Java program to print an American flag on the screen.
+ */
+
+
+public class UsaFlag {
+    public static void main(String[] args) {
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println(" * * * * *  ===================================");
+        System.out.println("* * * * * * ===================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+        System.out.println("===============================================");
+
+
+    }
+}
